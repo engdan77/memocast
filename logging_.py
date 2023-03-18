ba5716d8dfa1bfc67b71b4e9d3347595e246e691
Daniel Engval <daniel@engvalls.eu>
2023-03-18 13:57:21 +0100

    Improve logger

--- logging_.py
+++ logging_.py
@@ -3,6 +3,6 @@ from __info__ import __pkg__
 
 
 def get_logger():
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s: %(message)s')
     logger = logging.getLogger(__pkg__)
     return logger
