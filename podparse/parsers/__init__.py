bb6be446b1b8ecfb2a051295e93ecdfd6a3982b4
Daniel Engval <daniel@engvalls.eu>
2023-04-07 15:45:42 +0200

    Allow dynamically load all modules such as being plugins

--- podparse/parsers/__init__.py
+++ podparse/parsers/__init__.py
@@ -1,3 +1,7 @@
 import os
 import pkgutil
+import importlib
 __all__ = list(module for _, module, _ in pkgutil.iter_modules([os.path.dirname(__file__)]))  # Load all modules dynamically
+
+for module in (_ for _ in __all__ if not 'baseclass' in _):
+    importlib.import_module(f'.{module}', __package__)
