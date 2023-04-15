141beb0b4dccb706b06f32cd39288d114839c87c
Daniel Engval <daniel@engvalls.eu>
2023-04-15 10:39:33 +0200

    Basic installer

--- install.py
+++ install.py
@@ -1,3 +1,12 @@
+from urllib.request import urlopen
 from pathlib import Path
-
-...
+import zipfile
+url = "https://github.com/engdan77/memocast/archive/refs/heads/master.zip"
+zipf = "memocast.zip"
+with urlopen(url) as file:
+    content = file.read()
+with open(zipf, 'wb') as dl:
+    dl.write(content)
+with zipfile.ZipFile(zipf, "r") as zipr:
+    zipr.extractall(".")
+Path(zipf).unlink()
